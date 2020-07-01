avinashiitk/shr-meta-ros

meta-ros-common: wxwidgets, libfreenect: require also x11 in DISTRO_FEATURES
* fixes:
ERROR: Nothing PROVIDES 'wxwidgets' (but meta-ros/meta-ros-common/recipes-extended/wxwidgets/wxpython_3.0.2.0.bb DEPENDS on or otherwise requires it)
wxwidgets was skipped: missing required distro feature 'x11' (not in DISTRO_FEATURES)
ERROR: Nothing PROVIDES 'libxi' (but meta-ros/meta-ros-common/recipes-extended/libfreenect/libfreenect_0.5.2.bb DEPENDS on or otherwise requires it)
libxi was skipped: missing required distro feature 'x11' (not in DISTRO_FEATURES)
ERROR: Nothing PROVIDES 'libxmu' (but meta-ros/meta-ros-common/recipes-extended/libfreenect/libfreenect_0.5.2.bb DEPENDS on or otherwise requires it)
libxmu was skipped: missing required distro feature 'x11' (not in DISTRO_FEATURES)
ERROR: Nothing PROVIDES 'freeglut' (but meta-ros/meta-ros-common/recipes-extended/libfreenect/libfreenect_0.5.2.bb DEPENDS on or otherwise requires it)
freeglut was skipped: missing required distro feature 'x11' (not in DISTRO_FEATURES)

Signed-off-by: Martin Jansa <martin.jansa@lge.com>

meta-ros-common/recipes-extended/wxwidgets/wxpython_3.0.2.0.bb
@@ -36,3 +36,7 @@ do_iinstall_append() {
 }
 
 PNBLACKLIST[wxpython] ?= "${@bb.utils.contains('ROS_WORLD_SKIP_GROUPS', 'wxpython', 'Does not build: ld: cannot find -lwx_gtk3u_xrc-3.0', '', d)}"
+
+inherit distro_features_check
+# Depends on wxwidgets with this restriction:
+REQUIRED_DISTRO_FEATURES = "opengl x11"

victor-uavia/meta-ros

meta-ros-common/recipes-extended/wxwidgets/wxpython_3.0.2.0.bb
@@ -43,3 +43,7 @@ do_iinstall_append() {
 }
 
 PNBLACKLIST[wxpython] ?= "${@bb.utils.contains('ROS_WORLD_SKIP_GROUPS', 'wxpython', 'Does not build: ld: cannot find -lwx_gtk3u_xrc-3.0', '', d)}"
+
+inherit features_check
+# Depends on wxwidgets with this restriction:
+REQUIRED_DISTRO_FEATURES = "opengl x11"